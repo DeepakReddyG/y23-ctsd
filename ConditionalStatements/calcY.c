a5cde2021e6c6e1df07a334977d7a21e15dda3ad
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2023-08-23 18:09:22 +0530

    calcY program added

---
+++ ConditionalStatements/calcY.c
@@ -0,0 +1,12 @@
+//program to compute the value Y 
+//Y = (2*(a+b))+(a*b)
+
+#include<stdio.h>
+int main() {
+	int Y, a, b;
+	a = 4;
+	b = 7;
+	Y = (2*(a+b))+(a*b);
+	printf("%d", Y);
+	return 0;
+}
